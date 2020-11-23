12331ed047021d4e3671b218550f969b362b344e
zngsm <qbw0088@gmail.com>
2020-11-23 20:54:17 +0900

    adult icon - only recommend

--- src/views/Recommendation/Recommend.vue
+++ src/views/Recommendation/Recommend.vue
@@ -2,8 +2,13 @@
   <div>
     <h1>{{ user.username}} 님, 이런 영화는 어떠신가요?</h1>
     <div>
+      
       <iframe allow="autoplay;" width="560" height="315" :src="recommendMovieData.video" frameborder="0"></iframe>
-      <h2>{{recommendMovieData.title}}</h2>
+      <div class="d-flex">
+        <h2>{{recommendMovieData.title}}</h2>
+        <h1 v-if="recommendMovieData.adult" title="미성년관람불가">🔞</h1>
+        <h1 v-else title="미성년관람가능">⭕</h1>
+      </div>
       <h6>{{recommendMovieData.release_date}}</h6>
       <p>{{recommendMovieData.overview}}</p>
     </div>
